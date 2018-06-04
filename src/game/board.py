15165f7053e4c35212336d0570b0bfb909ec09f8
RuijieZ <r82zhang@edu.uwaterloo.ca>
2018-06-03 22:42:07 -0400

    Add multiple init.py, change the code structure a little bit, add 3 key functions, valid moves, range and threatened_moves

--- src/game/board.py
+++ src/game/board.py
@@ -1,54 +1,97 @@
 from functools import reduce
+from piece import King, Queen, Pawn, Knight, Rook, Bishop
+
+W = 'WHITE'
+B = 'BLACK'
+
+U = 'UP'
+D = 'Down'
 
 class Board(object):
-	"""Internal representation of a board
-	"""
-	def __init__(self):
-		super(Board, self).__init__()
-		self._board = [[None for i in range(8)] for _ in range(8)]
+    """Internal representation of a board
+    """
+    def __init__(self):
+        super(Board, self).__init__()
+        self._board = [[None for i in range(8)] for _ in range(8)]
+        self.cur_turn = W
+        self.w_direction = U
 
 
-	def board_is_valid(self):
-		return len(self._board) == 8 and reduce(lambda acc, x: acc and len(x) == 8, self._board, True)
+    # check if the board shape is not damaged by some bug in the code
+    def board_is_valid(self):
+        return len(self._board) == 8 and reduce(lambda acc, x: acc and len(x) == 8, self._board, True)
 
 
-	def get_piece(self, x, y):
-		return self._board[x][y]
+    # get a piece by coordinate
+    def get_piece(self, x, y):
+        return self._board[x][y]
 
 
-	# check move is valid in the given board situation
-	# first, call the api on the piece level, check if move is valid on the piece level
-	# then, check the board situation and determine whehter move is valid
-	def is_valid_move(self, c_x, c_y, t_x, t_y):
-		cur_piece = self.get_piece(c_x, c_y)
+    # get all pieces and coordinates of a ceratin colour
+    def get_pieces_by_colour(self, colour):
+        piece_lst = []
+        for row_num, r_lst in enumerate(self._board):
+            for col_num, piece in enumerate(r_lst):
+                if piece.colour == colour:
+                    piece_lst.append((row_num, col_num, piece))
 
-		for cord in [c_x, c_y, t_x, t_y]:
-			if cord < 0 or cord > 7:
-				return False
-
-		if not piece.is_valid_move(c_x, c_y, t_x, t_y):
-			return False
-
-		# board situation check if move is good
-		target_piece = self.get_piece(t_x, t_y)
-
-		# cannot move into your own piece
-		if target_piece is not None and target_piece.colour = cur_piece.colour:
-			return False
-
-		cors_in_path = cur_piece.cors_in_path(c_x, c_y, t_x, t_y)
+        return piece_lst
 
 
 
-	def remove_piece(self, x, y):
-		self._board[x][y] = None
+    def get_all_threatened_cors(self, colour):
+        piece_lst = self.get_piece_by_colour(colour)
+        for c_x, c_y, piece in piece_lst:
+            move_cors = piece.get_all_valid_moves(r_x, c)
+            cors_in_path = piece.cors_in_path()
 
 
-	def drop_piece(self, x, y, Piece):
-		self._board[x][y] = Piece
+
+    # check move is valid in the given board situation
+    # first, call the api on the piece level, check if move is valid on the piece level
+    # then, check the board situation and determine whehter move is valid
+    def is_valid_move(self, c_x, c_y, t_x, t_y):
+        cur_piece = self.get_piece(c_x, c_y)
+
+        for cord in [c_x, c_y, t_x, t_y]:
+            if cord < 0 or cord > 7:
+                return False
+
+        if not piece.is_valid_move(c_x, c_y, t_x, t_y):
+            return False
+
+        # board situation check if move is good
+        target_piece = self.get_piece(t_x, t_y)
+
+        # cannot move into your own piece
+        if target_piece is not None and target_piece.colour == cur_piece.colour:
+            return False
+
+        # cannot move if there is a piece block the piece for moving
+        cors_in_path = cur_piece.cors_in_path(c_x, c_y, t_x, t_y)
+        for x, y in cors_in_path:
+            if self.get_piece(x, y) is not None:
+                return False
+
+        # specail rules for king
+        # if cur_piece is King
+        if cur_piece is King:
+            opponent_colour = W if selfd.cur_turn == B else B
+            attacked_cors = self.get_all_moves_cors(opponent_colour)
+            if (t_x, t_y) in attacked_cors:
+                return False
+
+
+
+    def remove_piece(self, x, y):
+        self._board[x][y] = None
+
+
+    def drop_piece(self, x, y, Piece):
+        self._board[x][y] = Piece
 
 
 
 if __name__ == "__main__":
-	b = Board()
-	assert(b.board_is_valid())
+    b = Board()
+    assert(b.board_is_valid())
--- src/game/piece.py
+++ src/game/piece.py
@@ -1,53 +1,160 @@
+import board
+
 DEBUG = True
+
+W = 'WHITE'
+B = 'BLACK'
+
+L = 'LEFT'
+R = 'RIGTH'
+U = 'UP'
+D = 'DOWN'
+UL = 'UP_LEFT'
+UR = 'UP_RIGHT'
+DL = 'DOWN_LEFT'
+DR = 'DOWN_RIGHT'
+
+class PieceException(StandardError):
+	pass
 
 class Piece(object):
 	"""Master class of different chess pieces
 	"""
-	def __init__(self, board, colour):
+	def __init__(self, colour, board):
 		super(Piece, self).__init__()
-		self.board = board
 		self.colour = colour
+		self.board = board
 		self.has_moved = False
 
 	# this function implements a piece level is valid move check
-	# Note: a piece level valid move may not be a valid move on the board level
-	# since some pieces can block u in the board
-	# or a friendly piece is in the target position
+	# it checks whether the move is valid given the board situation
 	def is_valid_move(self, c_x, c_y, t_x, t_y):
-		return (t_x, t_y) in self.all_valid_moves(c_x, c_y)
+		return (t_x, t_y) in self.valid_moves(c_x, c_y)
 
-	# Again, this is a piece level valid moves
+
+	# this function returns the range of the given piece.
+	# that is, given no other pieces in the board, what is this piece can reach
+	# given the current position of the piece
 	# moves generate here may not be valid given the board situation
-	# the board level code will filter out more invalid moves
-	def all_valid_moves(self, c_x, c_y):
+	def range(self, c_x, c_y):
 		pass
+
+
+	# this function returns the cors that this piece is threatening,
+	# including the cors of the friendly pieces
+	# if a piece of another colour takes in this cors,
+	# then this piece can take away that piece
+	def threaten_cors(self, c_x, c_y):
+		return self.range(c_x, c_y)
+
+
+	# this function returns the cors that this piece can move to
+	def valid_moves(self, c_x, c_y):
+		t_cors = self.threaten_cors(c_x, c_y)
+		return self.remove_cors_occupied_by_friendly_piece(t_cors)
+
 
 	# remove duplicates in the cors
 	def remove_duplicates(self, list_of_cor):
 		return list(set(list_of_cor))
 
-	# filter out invalid cors
-	# here, it only filter out duplicate cors
-	# and OUT-OF-BOUNDARY cors.
-	# it DOES NOT filter out moves based on the board position
-	def filter_invalid_cors(self, list_of_cor, c_x, c_y):
-		list_of_cor = self.remove_duplicates(list_of_cor)
-		return list(filter(lambda c: 0 <= c[0] <= 7 and 0 <= c[1] <= 7 and c != (c_x, c_y), list_of_cor))
 
-	# this function give a list cordinates that will be in the "path" the movement
-	# the purpose of this function is to help the board chek whether there is a piece BLOCKING the move
-	# if there is a piece in the returned coordinates, then the move is invalid(blocked)
-	def cors_in_path(self, c_x, c_y, t_x, t_y):
-		return []
+	# return true if the c is within bound
+	def is_within_bound(self, c):
+		return 0 <= c[0] <= 7 and 0 <= c[1] <= 7
 
+
+	# filter out OUT-OF-BOUNDARY cors.
+	def filter_out_of_bound_cors(self, list_of_cor, c_x, c_y):
+		return [c for c in list_of_cor if self.is_within_bound(c) and c != (c_x, c_y)]
+
+
+	# given a coordinate, determine whether on that place is a friendly piece
+	def is_friendly_piece(self, c):
+		piece = self.board.get_piece(c[0], c[1])
+		return piece is not None and piece.colour == self.colour
+
+
+	def _threaten_cors_to_valid_moves(self, list_of_cor):
+		return [cor for cor in list_of_cor if not self.is_friendly_piece(cor)]
+
+
+	# including the closest_piece itself
+	def _cors_to_the_closest_piece(self, c_x, c_y, direction):
+		cors = []
+		if direction == L:
+			for i in range(c_y-1, -1, -1):
+				cors.append((c_x, i))
+				if self.board.get_piece(c_x, i) is not None:
+					break
+
+		elif direction == R:
+			for i in range(c_y+1, 8, 1):
+				cors.append((c_x, i))
+				if self.board.get_piece(c_x, i) is not None:
+					break
+
+		elif direction == U:
+			for i in range(c_x-1, -1, -1):
+				cors.append((i, c_y))
+				if self.board.get_piece(i, c_y) is not None:
+					break
+
+		elif direction == D:
+			for i in range(c_x+1, 8, 1):
+				cors.append((i, c_y))
+				if self.board.get_piece(i, c_y) is not None:
+					break
+
+		elif direction == UL:
+			for i in range(1, 8, 1):
+				c = (c_x - i, c_y - i)
+				if not self.is_within_bound(c):
+					break
+				cors.append(c)
+				if self.board.get_piece(c[0], c[1]) is not None:
+					break
+
+		elif direction == UR:
+			for i in range(1, 8, 1):
+				c = (c_x - i, c_y + i)
+				if not self.is_within_bound(c):
+					break
+				cors.append(c)
+				if self.board.get_piece(c[0], c[1]) is not None:
+					break
+
+		elif direction == DL:
+			for i in range(1, 8, 1):
+				c = (c_x + i, c_y - i)
+				if not self.is_within_bound(c):
+					break
+				cors.append(c)
+				if self.board.get_piece(c[0], c[1]) is not None:
+					break
+
+		elif direction == DR:
+			for i in range(1, 8, 1):
+				c = (c_x + i, c_y + i)
+				if not self.is_within_bound(c):
+					break
+				cors.append(c)
+				if self.board.get_piece(c[0], c[1]) is not None:
+					break
+
+		else:
+			raise PieceException("Invalid direction passed to _cors_to_the_closest_piece!")
+
+		return cors
 
 
 class Knight(Piece):
 	"""Knight"""
-	def __init__(self, board, colour):
-		super(Knight, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(Knight, self).__init__(colour, board)
 
-	def all_valid_moves(self, c_x, c_y):
+
+	def range(self, c_x, c_y):
 		cors = [
 			(c_x - 1, c_y + 2),
 			(c_x - 1, c_y - 2),
@@ -58,148 +165,142 @@ class Knight(Piece):
 			(c_x - 2, c_y + 1),
 			(c_x - 2, c_y - 1),
 		]
-		return self.filter_invalid_cors(cors, c_x, c_y)
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
 
 
 class Rook(Piece):
 	"""docstring for Rook"""
-	def __init__(self, board, colour):
-		super(Rook, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(Rook, self).__init__(colour, board)
 
 
-	def all_valid_moves(self, c_x, c_y):
+	def range(self, c_x, c_y):
 		x_change = [(c_x + k, c_y) for k in range(-7, 8)]
 		y_change = [(c_x, c_y + k) for k in range(-7, 8)]
-		cors = x_change + y_change
-		return self.filter_invalid_cors(cors, c_x, c_y)
+		cors = self.remove_duplicates(x_change + y_change)
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
 
-	def cors_in_path(self, c_x, c_y, t_x, t_y):
-		if DEBUG:
-			assert(self.is_valid_move(c_x, c_y, t_x, t_y))
 
-		if c_x == t_x:
-			return [(c_x, k) for k in range(min(c_y, t_y) + 1, max(c_y, t_y))]
-
-		else:
-			return [(k, c_y) for k in range(min(c_x, t_x) + 1, max(c_x, t_x))]
-
+	def threaten_cors(self, c_x, c_y):
+		left = self._cors_to_the_closest_piece(c_x, c_y, L)
+		right = self._cors_to_the_closest_piece(c_x, c_y, R)
+		up = self._cors_to_the_closest_piece(c_x, c_y, U)
+		down = self._cors_to_the_closest_piece(c_x, c_y, D)
+		return left + right + up + down
 
 
 class Bishop(Piece):
 	"""docstring for Bishop"""
-	def __init__(self, board, colour):
-		super(Bishop, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(Bishop, self).__init__(colour, board)
 
 
-	def all_valid_moves(self, c_x, c_y):
+	def range(self, c_x, c_y):
 		c1 = [(c_x + k, c_y + k) for k in range(-7, 8)]
 		c2 = [(c_x + k, c_y - k) for k in range(-7, 8)]
 		c3 = [(c_x - k, c_y + k) for k in range(-7, 8)]
 		c4 = [(c_x - k, c_y - k) for k in range(-7, 8)]
-		cors = c1 + c2 + c3 + c4
-		return self.filter_invalid_cors(cors, c_x, c_y)
+		cors = self.remove_duplicates(c1 + c2 + c3 + c4)
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
 
-	def cors_in_path(self, c_x, c_y, t_x, t_y):
-		if DEBUG:
-			assert(self.is_valid_move(c_x, c_y, t_x, t_y))
 
-		x_lst = range(min(c_x, t_x) + 1, max(c_x, t_x))
-		y_lst = range(min(c_y, t_y) + 1, max(c_y, t_y))
-		return list(zip(x_lst, y_lst))
+	def threaten_cors(self, c_x, c_y):
+		up_left = self._cors_to_the_closest_piece(c_x, c_y, UL)
+		up_right = self._cors_to_the_closest_piece(c_x, c_y, UR)
+		down_left = self._cors_to_the_closest_piece(c_x, c_y, DL)
+		down_right = self._cors_to_the_closest_piece(c_x, c_y, DR)
+		return up_left + up_right + down_left + down_right
 
 
 class Queen(Piece):
 	"""dtring for Queen"""
-	def __init__(self, board, colour):
-		super(Queen, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(Queen, self).__init__(colour, board)
 
 
-	def all_valid_moves(self, c_x, c_y):
+	def range(self, c_x, c_y):
 		c1 = [(c_x + k, c_y + k) for k in range(-7, 8)]
 		c2 = [(c_x + k, c_y - k) for k in range(-7, 8)]
 		c3 = [(c_x - k, c_y + k) for k in range(-7, 8)]
 		c4 = [(c_x - k, c_y - k) for k in range(-7, 8)]
 		c5 = [(c_x + k, c_y) for k in range(-7, 8)]
 		c6 = [(c_x, c_y + k) for k in range(-7, 8)]
-		cors = c1 + c2 + c3 + c4 + c5 + c6
-		return self.filter_invalid_cors(cors, c_x, c_y)
+		cors = self.remove_duplicates(c1 + c2 + c3 + c4 + c5 + c6)
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
 
 
-	def cors_in_path(self, c_x, c_y, t_x, t_y):
-		if DEBUG:
-			assert(self.is_valid_move(c_x, c_y, t_x, t_y))
+	def threaten_cors(self, c_x, c_y):
+		left = self._cors_to_the_closest_piece(c_x, c_y, L)
+		right = self._cors_to_the_closest_piece(c_x, c_y, R)
+		up = self._cors_to_the_closest_piece(c_x, c_y, U)
+		down = self._cors_to_the_closest_piece(c_x, c_y, D)
 
-		if c_x == t_x:
-			return [(c_x, k) for k in range(min(c_y, t_y) + 1, max(c_y, t_y))]
+		up_left = self._cors_to_the_closest_piece(c_x, c_y, UL)
+		up_right = self._cors_to_the_closest_piece(c_x, c_y, UR)
+		down_left = self._cors_to_the_closest_piece(c_x, c_y, DL)
+		down_right = self._cors_to_the_closest_piece(c_x, c_y, DR)
 
-		elif c_y == t_y:
-			return [(k, c_y) for k in range(min(c_x, t_x) + 1, max(c_x, t_x))]
-
-		x_lst = range(min(c_x, t_x) + 1, max(c_x, t_x))
-		y_lst = range(min(c_y, t_y) + 1, max(c_y, t_y))
-		return list(zip(x_lst, y_lst))
+		return left + right + up + down + up_left + up_right + down_left + down_right
 
 
 class Pawn(Piece):
-	def __init__(self, board, colour):
-		super(Pawn, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(Pawn, self).__init__(colour, board)
 
 
-	def all_valid_moves(self, c_x, c_y):
-		c1 = [(c_x - 1, c_y + k) for k in range(-1,2)]
-		c2 = [(c_x + 1, c_y + k) for k in range(-1,2)]
-		c3 = [(c_x + k, c_y) for k in (-2, 2)]
-		cors = c1 + c2 + c3
-		return self.filter_invalid_cors(cors, c_x, c_y)
+	def range(self, c_x, c_y):
+		# c1 = [(c_x - 1, c_y + k) for k in range(-1,2)]
+		# c2 = [(c_x + 1, c_y + k) for k in range(-1,2)]
+		# c3 = [(c_x + k, c_y) for k in (-2, 2)]
+		# cors = c1 + c2 + c3
 
-
-	def cors_in_path(self, c_x, c_y, t_x, t_y):
-		if DEBUG:
-			assert(self.is_valid_move(c_x, c_y, t_x, t_y))
-
-		if c_x == t_x:
-			return [(c_x, k) for k in range(min(c_y, t_y) + 1, max(c_y, t_y))]
-
-		else:
-			return [(k, c_y) for k in range(min(c_x, t_x) + 1, max(c_x, t_x))]
+		pass
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
 
 
 class King(Piece):
 	"""docstring for King"""
-	def __init__(self, board, colour):
-		super(King, self).__init__(board, colour)
+	def __init__(self, colour, board):
+		super(King, self).__init__(colour, board)
 
 
-	def all_valid_moves(self, c_x, c_y):
+	def range(self, c_x, c_y):
 		c1 = [(c_x - 1, c_y + k) for k in range(-1,2)]
 		c2 = [(c_x + 1, c_y + k) for k in range(-1,2)]
 		c3 = [(c_x, c_y + k) for k in (-1, 1)]
 		cors = c1 + c2 + c3
-		return self.filter_invalid_cors(cors, c_x, c_y)
+		return self.filter_out_of_bound_cors(cors, c_x, c_y)
+
+	def valid_moves(self, c_x, c_y):
+		opponent_colour = W if selfd.cur_turn == B else B  # determine the oppo colour
+		threaten_cors_by_opponent = self.board.get_all_threatened_cors(opponent_colour)  # get all cors are threatened by oppo
+		cors = self.remove_cors_occupied_by_friendly_piece(self.range(c_x, c_y))		# remove the cors that are ocupited by freindly piece
+		return [c for c in cors if c not in threaten_cors_by_opponent]
 
 
 if __name__ == "__main__":
-	kn = Knight([],'WHITE')
-	r = Rook([],'WHITE')
-	q = Queen([],'WHITE')
-	b = Bishop([],'WHITE')
-	k = King([], 'WHITE')
-	p = Pawn([], 'WHITE')
+	kn = Knight('WHITE', board.Board())
+	r = Rook('WHITE', board.Board())
+	q = Queen('WHITE', board.Board())
+	b = Bishop('WHITE', board.Board())
+	k = King('WHITE', board.Board())
+	p = Pawn('WHITE', board.Board())
 
 	# valid moves check
-	assert(sorted(kn.all_valid_moves(1,2)) == [(0, 0), (0, 4), (2, 0), (2, 4), (3, 1), (3, 3)])
-	assert(sorted(r.all_valid_moves(1,2)) == [(0, 2), (1, 0), (1, 1), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2)])
-	assert(sorted(q.all_valid_moves(1,2)) == [(0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (2, 1), (2, 2), (2, 3), (3, 0), (3, 2), (3, 4), (4, 2), (4, 5), (5, 2), (5, 6), (6, 2), (6, 7), (7, 2)])
-	assert(sorted(b.all_valid_moves(1,2)) == [(0, 1), (0, 3), (2, 1), (2, 3), (3, 0), (3, 4), (4, 5), (5, 6), (6, 7)])
-	assert(sorted(k.all_valid_moves(1,2)) == [(0, 1), (0, 2), (0, 3), (1, 1), (1, 3), (2, 1), (2, 2), (2, 3)])
-	assert(sorted(p.all_valid_moves(1,2)) == [(0, 1), (0, 2), (0, 3), (2, 1), (2, 2), (2, 3), (3, 2)])
+	assert(sorted(kn.range(1,2)) == [(0, 0), (0, 4), (2, 0), (2, 4), (3, 1), (3, 3)])
+	assert(sorted(r.range(1,2)) == [(0, 2), (1, 0), (1, 1), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2)])
+	assert(sorted(q.range(1,2)) == [(0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (2, 1), (2, 2), (2, 3), (3, 0), (3, 2), (3, 4), (4, 2), (4, 5), (5, 2), (5, 6), (6, 2), (6, 7), (7, 2)])
+	assert(sorted(b.range(1,2)) == [(0, 1), (0, 3), (2, 1), (2, 3), (3, 0), (3, 4), (4, 5), (5, 6), (6, 7)])
+	assert(sorted(k.range(1,2)) == [(0, 1), (0, 2), (0, 3), (1, 1), (1, 3), (2, 1), (2, 2), (2, 3)])
+	assert(sorted(p.range(1,2)) == [(0, 1), (0, 2), (0, 3), (2, 1), (2, 2), (2, 3), (3, 2)])
 
-	# cors in the path
-	assert(r.cors_in_path(1, 4, 1, 1) == [(1, 2), (1, 3)])
-	assert(b.cors_in_path(4, 6, 1, 3) == [(2, 4), (3, 5)])
-	assert(q.cors_in_path(4, 6, 4, 3) == [(4, 4), (4, 5)])
-	assert(p.cors_in_path(6, 6, 4, 6) == [(5,6)])
-
-
-
+	# closest piece tests
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, L)) == [(1,0), (1,1)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, R)) == [(1,3), (1,4), (1,5), (1,6), (1,7)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, U)) == [(0,2)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, D)) == [(2,2), (3,2), (4,2), (5,2), (6,2), (7,2)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, UL)) == [(0,1)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, UR)) == [(0,3)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, DL)) == [(2,1), (3,0)])
+	assert(sorted(kn._cors_to_the_closest_piece(1,2, DR)) == [(2,3), (3,4), (4,5), (5,6), (6,7)])
 

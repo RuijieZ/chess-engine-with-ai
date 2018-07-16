1e7b9c8919ae2ea20dd36be9eed46977cba070cd
RuijieZ <r82zhang@edu.uwaterloo.ca>
2018-07-16 04:44:27 -0400

    try to improve move ordering

--- src/ai/alpha_beta.py
+++ src/ai/alpha_beta.py
@@ -15,12 +15,63 @@ pv_table_dict = {}				# key: board_fen+min/max, value: move
 move_cutoff_table = {}			# key: board_fen+min/max. value: move
 move_ordering_cache = {}		# key: board_fen+min/max, value: move
 
+file_map = {
+	'a': 0,
+	'b': 1,
+	'c': 2,
+	'd': 3,
+	'e': 4,
+	'f': 5,
+	'g': 6,
+	'h': 7,
+}
+
+rank_map = {
+	'1': 0,
+	'2': 1,
+	'3': 2,
+	'4': 3,
+	'5': 4,
+	'6': 5,
+	'7': 6,
+	'8': 7,
+}
+
+piece_type_map = {
+	1: 1,
+	2: 2,
+	3: 2,
+	4: 4,
+	5: 5,
+	6: 6,
+	None: 1
+}
+
+# for i in range(1,9):
+# 	for idx, j in enumerate('abcdefgh'):
+# 		pos = (i-1) * 8 + (7-idx)
+# 		print("\'" + j + str(i) + "\'" + ": " + str(pos) + ",")
+
+def calculate_cap_move_score(board, move):
+	uci = move.uci()
+	from_sq, to_sq = chess.square(file_map[uci[0]], rank_map[uci[1]]) , chess.square(file_map[uci[2]], rank_map[uci[3]])
+	source, victim = piece_type_map[board.piece_type_at(from_sq)], piece_type_map[board.piece_type_at(to_sq)]
+	return source  - victim    # the smaller the better the cap move
+
+
+
+def order_capture_moves(board, capture_moves):
+	capture_moves.sort(key=lambda m: calculate_cap_move_score(board, m))
+	return capture_moves
+
+
 def move_ordering(board, is_max, key, s_move):
 	# try to get basic moves
 	try:
 		moves = move_ordering_cache[key]
 	except KeyError as e:
-		capture_moves = list(board.generate_legal_captures())
+		capture_moves = order_capture_moves(board, list(board.generate_legal_captures()))
+		# capture_moves = list(board.generate_legal_captures())
 		all_moves = list(board.legal_moves)
 		rest_moves = list(set(all_moves) - set(capture_moves))
 		moves = capture_moves + rest_moves			# make capture moves in the front
@@ -83,7 +134,6 @@ def min_f(board, alpha, beta, depth):
 		for move in moves:
 			board.push(move)	# make that move to make a new board
 			v = max_f(board, alpha, beta, depth-1)
-
 			board.pop() 		# unmake that move
 
 			if v < best_value:
@@ -162,9 +212,9 @@ if __name__ == '__main__':
 	# print(min_f(board, -10001, 10001, 4))
 	# print(pv_table_dict[board.board_fen()+'_min'])
 
-	for i in range(7):
+	for i in range(8):
 		min_f(board, -10001, 10001, i)
-	# print(pv_table_dict[board.board_fen()+'_min'])
+	print(previous_result_dict[board.board_fen()+'_min'])
 
 	# print(iterative_deepening(board, -10001, 10001, 5 ,False))
 	# print(min_f(board, 2, 2))

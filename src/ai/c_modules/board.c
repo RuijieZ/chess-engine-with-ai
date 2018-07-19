20171532da38ed0a902f6cbfd3692b34878bdde4
RuijieZ <r82zhang@edu.uwaterloo.ca>
2018-07-19 08:48:50 -0400

    board.c fix

--- src/ai/c_modules/board.c
+++ src/ai/c_modules/board.c
@@ -1,31 +1,52 @@
-#include <stdio.h>
+// board.c
+
+#include "stdio.h"
 #include "defs.h"
 
-int CheckBoard(const S_BOARD *pos) {   
- 
+int PceListOk(const S_BOARD *pos) {
+	int pce = wP;
+	int sq;
+	int num;
+	for(pce = wP; pce <= bK; ++pce) {
+		if(pos->pceNum[pce]<0 || pos->pceNum[pce]>=10) return FALSE;
+	}
+
+	if(pos->pceNum[wK]!=1 || pos->pceNum[bK]!=1) return FALSE;
+
+	for(pce = wP; pce <= bK; ++pce) {
+		for(num = 0; num < pos->pceNum[pce]; ++num) {
+			sq = pos->pList[pce][num];
+			if(!SqOnBoard(sq)) return FALSE;
+		}
+	}
+    return TRUE;
+}
+
+int CheckBoard(const S_BOARD *pos) {
+
 	int t_pceNum[13] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	int t_bigPce[2] = { 0, 0};
 	int t_majPce[2] = { 0, 0};
 	int t_minPce[2] = { 0, 0};
 	int t_material[2] = { 0, 0};
-	
+
 	int sq64,t_piece,t_pce_num,sq120,colour,pcount;
-	
+
 	U64 t_pawns[3] = {0ULL, 0ULL, 0ULL};
-	
+
 	t_pawns[WHITE] = pos->pawns[WHITE];
 	t_pawns[BLACK] = pos->pawns[BLACK];
 	t_pawns[BOTH] = pos->pawns[BOTH];
-	
+
 	// check piece lists
 	for(t_piece = wP; t_piece <= bK; ++t_piece) {
 		for(t_pce_num = 0; t_pce_num < pos->pceNum[t_piece]; ++t_pce_num) {
 			sq120 = pos->pList[t_piece][t_pce_num];
 			ASSERT(pos->pieces[sq120]==t_piece);
-		}	
+		}
 	}
-	
-	// check piece count and other counters	
+
+	// check piece count and other counters
 	for(sq64 = 0; sq64 < 64; ++sq64) {
 		sq120 = SQ120(sq64);
 		t_piece = pos->pieces[sq120];
@@ -34,14 +55,14 @@ int CheckBoard(const S_BOARD *pos) {
 		if( PieceBig[t_piece] == TRUE) t_bigPce[colour]++;
 		if( PieceMin[t_piece] == TRUE) t_minPce[colour]++;
 		if( PieceMaj[t_piece] == TRUE) t_majPce[colour]++;
-		
+
 		t_material[colour] += PieceVal[t_piece];
 	}
-	
+
 	for(t_piece = wP; t_piece <= bK; ++t_piece) {
-		ASSERT(t_pceNum[t_piece]==pos->pceNum[t_piece]);	
+		ASSERT(t_pceNum[t_piece]==pos->pceNum[t_piece]);
 	}
-	
+
 	// check bitboards count
 	pcount = CNT(t_pawns[WHITE]);
 	ASSERT(pcount == pos->pceNum[wP]);
@@ -49,105 +70,42 @@ int CheckBoard(const S_BOARD *pos) {
 	ASSERT(pcount == pos->pceNum[bP]);
 	pcount = CNT(t_pawns[BOTH]);
 	ASSERT(pcount == (pos->pceNum[bP] + pos->pceNum[wP]));
-	
+
 	// check bitboards squares
 	while(t_pawns[WHITE]) {
 		sq64 = POP(&t_pawns[WHITE]);
 		ASSERT(pos->pieces[SQ120(sq64)] == wP);
 	}
-	
+
 	while(t_pawns[BLACK]) {
 		sq64 = POP(&t_pawns[BLACK]);
 		ASSERT(pos->pieces[SQ120(sq64)] == bP);
 	}
-	
+
 	while(t_pawns[BOTH]) {
 		sq64 = POP(&t_pawns[BOTH]);
 		ASSERT( (pos->pieces[SQ120(sq64)] == bP) || (pos->pieces[SQ120(sq64)] == wP) );
 	}
-	
+
 	ASSERT(t_material[WHITE]==pos->material[WHITE] && t_material[BLACK]==pos->material[BLACK]);
 	ASSERT(t_minPce[WHITE]==pos->minPce[WHITE] && t_minPce[BLACK]==pos->minPce[BLACK]);
 	ASSERT(t_majPce[WHITE]==pos->majPce[WHITE] && t_majPce[BLACK]==pos->majPce[BLACK]);
-	ASSERT(t_bigPce[WHITE]==pos->bigPce[WHITE] && t_bigPce[BLACK]==pos->bigPce[BLACK]);	
-	
+	ASSERT(t_bigPce[WHITE]==pos->bigPce[WHITE] && t_bigPce[BLACK]==pos->bigPce[BLACK]);
+
 	ASSERT(pos->side==WHITE || pos->side==BLACK);
 	ASSERT(GeneratePosKey(pos)==pos->posKey);
-	
+
 	ASSERT(pos->enPas==NO_SQ || ( RanksBrd[pos->enPas]==RANK_6 && pos->side == WHITE)
 		 || ( RanksBrd[pos->enPas]==RANK_3 && pos->side == BLACK));
-	
+
 	ASSERT(pos->pieces[pos->KingSq[WHITE]] == wK);
 	ASSERT(pos->pieces[pos->KingSq[BLACK]] == bK);
-		 
-	return TRUE;	
-}
 
-void PrintBoard(const S_BOARD *pos) {
+	ASSERT(pos->castlePerm >= 0 && pos->castlePerm <= 15);
 
-	int sq,file,rank,piece;
+	ASSERT(PceListOk(pos));
 
-	printf("\nGame Board:\n\n");
-
-	for(rank = RANK_8; rank >= RANK_1; rank--) {
-		printf("%d  ",rank+1);
-		for(file = FILE_A; file <= FILE_H; file++) {
-			sq = FR2SQ(file,rank);
-			piece = pos->pieces[sq];
-			printf("%3c",PceChar[piece]);
-		}
-		printf("\n");
-	}
-
-	printf("\n   ");
-	for(file = FILE_A; file <= FILE_H; file++) {
-		printf("%3c",'a'+file);
-	}
-	printf("\n");
-	printf("side:%c\n",SideChar[pos->side]);
-	printf("enPas:%d\n",pos->enPas);
-	printf("castle:%c%c%c%c\n",
-			pos->castlePerm & WKCA ? 'K' : '-',
-			pos->castlePerm & WQCA ? 'Q' : '-',
-			pos->castlePerm & BKCA ? 'k' : '-',
-			pos->castlePerm & BQCA ? 'q' : '-'
-			);
-	printf("PosKey:%llX\n",pos->posKey);
-}
-
-void ResetBoard(S_BOARD *pos) {
-
-	for (int i = 0; i < BRD_SQ_NUM; ++i) {
-		pos->pieces[i] = OFFBOARD;
-	}
-
-	for (int i = 0; i < 64; ++i) {
-		pos->pieces[SQ120(i)] = EMPTY;
-	}
-
-	for (int i = 0; i < 3; ++i){
-		pos->bigPce[i] = 0;
-		pos->majPce[i] = 0;
-		pos->minPce[i] = 0;
-		pos->pawns[i] = 0ULL;
-	}
-
-	for (int i = 0; i < 13; ++i) {
-		pos->pceNum[i] = 0;
-	}
-
-	pos->KingSq[WHITE] = pos->KingSq[BLACK] = NO_SQ;
-
-	pos->side = BOTH;
-	pos->enPas = NO_SQ;
-	pos->fiftyMove = 0;
-
-	pos->ply = 0;
-	pos->hisPly = 0;
-
-	pos->castlePerm = 0;
-
-	pos->posKey = 0ULL;
+	return TRUE;
 }
 
 void UpdateListsMaterial(S_BOARD *pos) {
@@ -157,8 +115,10 @@ void UpdateListsMaterial(S_BOARD *pos) {
 	for(index = 0; index < BRD_SQ_NUM; ++index) {
 		sq = index;
 		piece = pos->pieces[index];
+		ASSERT(PceValidEmptyOffbrd(piece));
 		if(piece!=OFFBOARD && piece!= EMPTY) {
 			colour = PieceCol[piece];
+			ASSERT(SideValid(colour));
 
 		    if( PieceBig[piece] == TRUE) pos->bigPce[colour]++;
 		    if( PieceMin[piece] == TRUE) pos->minPce[colour]++;
@@ -166,11 +126,14 @@ void UpdateListsMaterial(S_BOARD *pos) {
 
 			pos->material[colour] += PieceVal[piece];
 
+			ASSERT(pos->pceNum[piece] < 10 && pos->pceNum[piece] >= 0);
+
 			pos->pList[piece][pos->pceNum[piece]] = sq;
 			pos->pceNum[piece]++;
 
+
 			if(piece==wK) pos->KingSq[WHITE] = sq;
-			if(piece==bK) pos->KingSq[BLACK] = sq;	
+			if(piece==bK) pos->KingSq[BLACK] = sq;
 
 			if(piece==wP) {
 				SETBIT(pos->pawns[WHITE],SQ64(sq));
@@ -184,20 +147,20 @@ void UpdateListsMaterial(S_BOARD *pos) {
 }
 
 int ParseFen(char *fen, S_BOARD *pos) {
-	
+
 	ASSERT(fen!=NULL);
 	ASSERT(pos!=NULL);
-	
+
 	int  rank = RANK_8;
     int  file = FILE_A;
     int  piece = 0;
     int  count = 0;
-    int  i = 0; 
-	int  sq64 = 0; 
+    int  i = 0;
+	int  sq64 = 0;
 	int  sq120 = 0;
-	
+
 	ResetBoard(pos);
-	
+
 	while ((rank >= RANK_1) && *fen) {
 	    count = 1;
 		switch (*fen) {
@@ -231,14 +194,14 @@ int ParseFen(char *fen, S_BOARD *pos) {
                 rank--;
                 file = FILE_A;
                 fen++;
-                continue;              
+                continue;
 
             default:
                 printf("FEN error \n");
                 return -1;
-        }		
-		
-		for (i = 0; i < count; i++) {			
+        }
+
+		for (i = 0; i < count; i++) {
             sq64 = rank * 8 + file;
 			sq120 = SQ120(sq64);
             if (piece != EMPTY) {
@@ -248,16 +211,16 @@ int ParseFen(char *fen, S_BOARD *pos) {
         }
 		fen++;
 	}
-	
+
 	ASSERT(*fen == 'w' || *fen == 'b');
-	
+
 	pos->side = (*fen == 'w') ? WHITE : BLACK;
 	fen += 2;
-	
+
 	for (i = 0; i < 4; i++) {
         if (*fen == ' ') {
             break;
-        }		
+        }
 		switch(*fen) {
 			case 'K': pos->castlePerm |= WKCA; break;
 			case 'Q': pos->castlePerm |= WQCA; break;
@@ -268,22 +231,138 @@ int ParseFen(char *fen, S_BOARD *pos) {
 		fen++;
 	}
 	fen++;
-	
+
 	ASSERT(pos->castlePerm>=0 && pos->castlePerm <= 15);
-	
-	if (*fen != '-') {        
+
+	if (*fen != '-') {
 		file = fen[0] - 'a';
 		rank = fen[1] - '1';
-		
+
 		ASSERT(file>=FILE_A && file <= FILE_H);
 		ASSERT(rank>=RANK_1 && rank <= RANK_8);
-		
-		pos->enPas = FR2SQ(file,rank);		
+
+		pos->enPas = FR2SQ(file,rank);
     }
-	
-	pos->posKey = GeneratePosKey(pos); 
-	
+
+	pos->posKey = GeneratePosKey(pos);
+
 	UpdateListsMaterial(pos);
-	
+
 	return 0;
+}
+
+void ResetBoard(S_BOARD *pos) {
+
+	int index = 0;
+
+	for(index = 0; index < BRD_SQ_NUM; ++index) {
+		pos->pieces[index] = OFFBOARD;
+	}
+
+	for(index = 0; index < 64; ++index) {
+		pos->pieces[SQ120(index)] = EMPTY;
+	}
+
+	for(index = 0; index < 2; ++index) {
+		pos->bigPce[index] = 0;
+		pos->majPce[index] = 0;
+		pos->minPce[index] = 0;
+		pos->material[index] = 0;
+	}
+
+	for(index = 0; index < 3; ++index) {
+		pos->pawns[index] = 0ULL;
+	}
+
+	for(index = 0; index < 13; ++index) {
+		pos->pceNum[index] = 0;
+	}
+
+	pos->KingSq[WHITE] = pos->KingSq[BLACK] = NO_SQ;
+
+	pos->side = BOTH;
+	pos->enPas = NO_SQ;
+	pos->fiftyMove = 0;
+
+	pos->ply = 0;
+	pos->hisPly = 0;
+
+	pos->castlePerm = 0;
+
+	pos->posKey = 0ULL;
+
+}
+void PrintBoard(const S_BOARD *pos) {
+
+	int sq,file,rank,piece;
+
+	printf("\nGame Board:\n\n");
+
+	for(rank = RANK_8; rank >= RANK_1; rank--) {
+		printf("%d  ",rank+1);
+		for(file = FILE_A; file <= FILE_H; file++) {
+			sq = FR2SQ(file,rank);
+			piece = pos->pieces[sq];
+			printf("%3c",PceChar[piece]);
+		}
+		printf("\n");
+	}
+
+	printf("\n   ");
+	for(file = FILE_A; file <= FILE_H; file++) {
+		printf("%3c",'a'+file);
+	}
+	printf("\n");
+	printf("side:%c\n",SideChar[pos->side]);
+	printf("enPas:%d\n",pos->enPas);
+	printf("castle:%c%c%c%c\n",
+			pos->castlePerm & WKCA ? 'K' : '-',
+			pos->castlePerm & WQCA ? 'Q' : '-',
+			pos->castlePerm & BKCA ? 'k' : '-',
+			pos->castlePerm & BQCA ? 'q' : '-'
+			);
+	printf("PosKey:%llX\n",pos->posKey);
+}
+
+void MirrorBoard(S_BOARD *pos) {
+
+    int tempPiecesArray[64];
+    int tempSide = pos->side^1;
+	int SwapPiece[13] = { EMPTY, bP, bN, bB, bR, bQ, bK, wP, wN, wB, wR, wQ, wK };
+    int tempCastlePerm = 0;
+    int tempEnPas = NO_SQ;
+
+	int sq;
+	int tp;
+
+    if (pos->castlePerm & WKCA) tempCastlePerm |= BKCA;
+    if (pos->castlePerm & WQCA) tempCastlePerm |= BQCA;
+
+    if (pos->castlePerm & BKCA) tempCastlePerm |= WKCA;
+    if (pos->castlePerm & BQCA) tempCastlePerm |= WQCA;
+
+	if (pos->enPas != NO_SQ)  {
+        tempEnPas = SQ120(Mirror64[SQ64(pos->enPas)]);
+    }
+
+    for (sq = 0; sq < 64; sq++) {
+        tempPiecesArray[sq] = pos->pieces[SQ120(Mirror64[sq])];
+    }
+
+    ResetBoard(pos);
+
+	for (sq = 0; sq < 64; sq++) {
+        tp = SwapPiece[tempPiecesArray[sq]];
+        pos->pieces[SQ120(sq)] = tp;
+    }
+
+	pos->side = tempSide;
+    pos->castlePerm = tempCastlePerm;
+    pos->enPas = tempEnPas;
+
+    pos->posKey = GeneratePosKey(pos);
+
+	UpdateListsMaterial(pos);
+
+    ASSERT(CheckBoard(pos));
 }

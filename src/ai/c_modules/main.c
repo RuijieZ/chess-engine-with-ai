fdbc495f2b73c43ccd44f2a96829a320f5ee7431
RuijieZ <r82zhang@edu.uwaterloo.ca>
2018-07-19 05:42:03 -0400

    more tuning

--- src/ai/c_modules/main.c
+++ src/ai/c_modules/main.c
@@ -332,9 +332,9 @@ int AlphaBeta(S_BOARD *pos, int alpha, int beta, int depth, int colour) {
 		// set some search parameters
 		if (board->material[board->side] <= ENDGAME_MAT) {	// END GAME
 			printf("End Game\n");
-			BRANCH_REDUCE_FACTOR = 4;
-			REDUCE_DEPTH = 4;
-			SEARCH_DEPTH = 12;
+			BRANCH_REDUCE_FACTOR = 6;
+			REDUCE_DEPTH = 8;
+			SEARCH_DEPTH = 16;
 		} else {
 			printf("NOT End Game\n");			// NOT ENDING
 			BRANCH_REDUCE_FACTOR = 6;

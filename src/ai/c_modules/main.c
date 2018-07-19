9a62cf328a7cc08927aa8f345ade08e1ccea8de0
RuijieZ <r82zhang@edu.uwaterloo.ca>
2018-07-19 06:21:49 -0400

    no message

--- src/ai/c_modules/main.c
+++ src/ai/c_modules/main.c
@@ -339,14 +339,14 @@ int AlphaBeta(S_BOARD *pos, int alpha, int beta, int depth, int colour, struct I
 		// set some search parameters
 		if (board->material[board->side] <= ENDGAME_MAT) {	// END GAME
 			printf("End Game\n");
-			BRANCH_REDUCE_FACTOR = 3;
-			REDUCE_DEPTH = 8;
-			SEARCH_DEPTH = 16;
+			BRANCH_REDUCE_FACTOR = 1;
+			REDUCE_DEPTH = 0;
+			SEARCH_DEPTH = 12;
 		} else {
 			printf("NOT End Game\n");			// NOT ENDING
-			BRANCH_REDUCE_FACTOR = 4;
-			REDUCE_DEPTH = 4;
-			SEARCH_DEPTH = 12;
+			BRANCH_REDUCE_FACTOR = 1;
+			REDUCE_DEPTH = 0;
+			SEARCH_DEPTH = 10;
 		}
 
 		rootPoskey = board->posKey;
